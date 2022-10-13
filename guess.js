ff30d5fb7efc7a610c52f6bb1bf24f3ee8b2fad4
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-13 09:35:41 -0400

    2nd

---
+++ guess.js
@@ -0,0 +1,10 @@
+/*
+
+create program that guesses a number from 1 to 10
+then asks us to guess that number 
+if its higher say 'to  high' and ask again
+if its lower say "to low" and ask again 
+if its the right number say "congratulations" and exit
+
+*/
+import readline from "readline-sync"

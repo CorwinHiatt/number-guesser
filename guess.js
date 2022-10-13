ce9011e9e855bbfd076845d9194a3f11a733ffec
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-13 10:21:06 -0400

    2nd

--- guess.js
+++ guess.js
@@ -8,3 +8,40 @@ if its the right number say "congratulations" and exit
 
 */
 import readline from "readline-sync"
+
+let userGuessRight = false
+
+//while(true){//pointless
+//}
+
+    //guess # from 1-10
+let NumberChosen = Math.ceil(Math.random() * 10)
+                                                    //console.log(NumberChosen)
+
+while(!userGuessRight){
+let guess = readline.question("what is your guess")
+if (guess == NumberChosen)
+{
+    console.log("congratulations!!! You space monkey")
+    userGuessRight = true
+}
+if (guess > NumberChosen)
+{
+    console.log("to  high! Try again")
+    //userGuessRight=false
+}
+if (guess < NumberChosen)
+{
+    console.log("to low! Try again")
+   // userGuessRight=false
+}
+                                                    //console.log(guess)
+
+}
+                                                    
+                                                    //while(!userGuessRight) {
+                                                    //   console.log("what is your guess")
+                                                    //   break  
+                                                    //}
+                                                    
+                                                    
